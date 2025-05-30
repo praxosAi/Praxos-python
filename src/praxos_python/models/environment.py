fa55723b17ccad3d58b7944c4aed677c2f8569c8
masoud <masoudkermanipoor@gmail.com>
2025-05-30 11:11:37 -0400

    make argument name of environment constant

--- src/praxos_python/models/environment.py
+++ src/praxos_python/models/environment.py
@@ -46,7 +46,7 @@ class SyncEnvironment(BaseEnvironmentAttributes):
         else:
             return contexts
 
-    def add_conversation(self, messages: List[Message|Dict[str, str]], source_name: str=None, description: str=None) -> SyncSource:
+    def add_conversation(self, messages: List[Message|Dict[str, str]], name: str=None, description: str=None) -> SyncSource:
         """Adds a conversation source."""
         if len(messages) == 0:
             raise ValueError("Messages must be a non-empty list")
@@ -58,25 +58,25 @@ class SyncEnvironment(BaseEnvironmentAttributes):
             "description": description
         }
 
-        if source_name:
-            payload["name"] = source_name
+        if name:
+            payload["name"] = name
 
         response_data = self._client._request("POST", f"/sources", params={"type": "conversation", "environment_id": self.id}, json_data=payload)
         return SyncSource(client=self._client, **response_data)
 
-    def add_file(self, file_path: str, name: str=None, description: str=None) -> SyncSource:
+    def add_file(self, path: str, name: str=None, description: str=None) -> SyncSource:
         """Adds a file source."""
         global ACCEPTABLE_SOURCE_EXTENSIONS_TO_CONTENT_TYPE
 
-        if not os.path.exists(file_path):
-            raise FileNotFoundError(f"File not found: {file_path}")
+        if not os.path.exists(path):
+            raise FileNotFoundError(f"File not found: {path}")
 
-        file_extension = file_path.split('.')[-1]
+        file_extension = path.split('.')[-1]
         if file_extension not in ACCEPTABLE_SOURCE_EXTENSIONS_TO_CONTENT_TYPE:
             raise ValueError(f"File extension {file_extension} is not supported. Supported extensions are: {', '.join(ACCEPTABLE_SOURCE_EXTENSIONS_TO_CONTENT_TYPE.keys())}")
         
         try:
-            with open(file_path, 'rb') as f:
+            with open(path, 'rb') as f:
                 files = {'file': (name, f, ACCEPTABLE_SOURCE_EXTENSIONS_TO_CONTENT_TYPE[file_extension])}
                 form_data = {"type": "file", "name": name, "description": description}
                 response_data = self._client._request(
@@ -84,7 +84,7 @@ class SyncEnvironment(BaseEnvironmentAttributes):
                 )
             return SyncSource(client=self._client, **response_data)
         except FileNotFoundError:
-            raise ValueError(f"File not found: {file_path}")
+            raise ValueError(f"File not found: {path}")
         except Exception as e:
             raise APIError(status_code=0, message=f"Sync file upload failed: {str(e)}") from e
         
@@ -104,14 +104,14 @@ class SyncEnvironment(BaseEnvironmentAttributes):
         response_data = self._client._request("GET", f"/sources", params={"environment_id": self.id})
         return [SyncSource(client=self._client, **source) for source in response_data]
 
-    def get_source(self, source_id: str=None, source_name: str=None) -> SyncSource:
+    def get_source(self, id: str=None, name: str=None) -> SyncSource:
         """Gets a source for the environment."""
-        if source_id is None and source_name is None:
-            raise ValueError("Either source_id or source_name must be provided")
+        if id is None and name is None:
+            raise ValueError("Either id or name must be provided")
         
-        if source_id:
-            response_data = self._client._request("GET", f"/sources", params={"environment_id": self.id, "id": source_id})
+        if id:
+            response_data = self._client._request("GET", f"/sources", params={"environment_id": self.id, "id": id})
         else:
-            response_data = self._client._request("GET", f"/sources", params={"environment_id": self.id, "name": source_name})
+            response_data = self._client._request("GET", f"/sources", params={"environment_id": self.id, "name": name})
 
         return SyncSource(client=self._client, **response_data)
